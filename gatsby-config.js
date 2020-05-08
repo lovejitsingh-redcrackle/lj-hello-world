ebfdfe44b7d2363c32ca090031179f995c8bfd8a
Lovejit Singh <lovejit.singh@redcrackle.com>
2020-05-08 15:29:31 +0530

    remvoing extra plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,7 @@ module.exports = {
       options: {
         baseUrl: `https://dev-gatsby-lj.pantheonsite.io/`,
         apiBase: `jsonapi`, // optional, defaults to `jsonapi`
+        secret: "ec64445c-5a98-4462-b4a9-d2b6d0ca0422",
         basicAuth: {
           username: "restws.admin",
           password: "admin123",
